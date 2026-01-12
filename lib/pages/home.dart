62724249a4e6363560ed5e2312812bd770f5feb4
Fathur <muhammadfathurrahman013@gmail.com>
2026-01-13 00:16:27 +0700

    create home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_api/model/brainrots.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter_api/widget/post_card.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<List<Brainrots>> fetchPosts() async {
+    final response =
+        await http.get(Uri.parse('https://raw.githubusercontent.com/fyou00/brainrot-api/refs/heads/main/brainrot.json'));
+
+    print(response.statusCode);
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      var getPostsData = json.decode(response.body) as List;
+      var listPosts = getPostsData.map((i) => Brainrots.fromJson(i)).toList();
+      return listPosts;
+    } else {
+      throw Exception('Failed to load Posts');
+    }
+  }
+
+  late Future<List<Brainrots>> futurePosts;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePosts = fetchPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Post List'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: FutureBuilder<List<Brainrots>>(
+              future: futurePosts,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.separated(
+                      itemBuilder: ((context, index) {
+                        var post = (snapshot.data as List<Brainrots>)[index];
+                        return Column(
+                          children: [
+                            PostCard(
+                              brainrots: Brainrots(
+                                  id: post.id,
+                                  name: post.name,
+                                  body: post.body),
+                            ),
+                            SizedBox(height: 20)
+                          ],
+                        );
+                      }),
+                      separatorBuilder: (context, index) {
+                        return const Divider();
+                      },
+                      itemCount: (snapshot.data as List<Brainrots>).length);
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return const CircularProgressIndicator();
+              }),
+        ),
+      ),
+    );
+  }
+}
