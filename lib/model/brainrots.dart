81ab3f9c26993a52d65f4d423275d971747899c9
Fathur <muhammadfathurrahman013@gmail.com>
2026-01-14 00:25:56 +0700

    tes variabel

--- lib/model/brainrots.dart
+++ lib/model/brainrots.dart
@@ -6,10 +6,24 @@ class Brainrots {
   Brainrots({required this.id, required this.name, required this.body});
 
   factory Brainrots.fromJson(Map<String, dynamic> json) {
-    return Brainrots(
-      id: json['id'],
-      name: json['name'],
-      body: json['body'],
-    );
+    return Brainrots(id: json['id'], name: json['name'], body: json['body']);
   }
+}
+
+void main() {
+  Map<String, dynamic> jsonData = {
+    'namaDepan': 'Ballerina',
+    'namaBelakang': 'Cappuccina'
+  };
+
+  String namaDepan = jsonData['namaDepan'];
+  String namaBelakang = jsonData['namaBelakang'];
+
+  String callNamaPanjang(String namaDepan, String namaBelakang) {
+    String namaPanjang = '$namaDepan' + ' ' + '$namaBelakang';
+    return namaPanjang;
+  }
+
+  String namaPanjang = callNamaPanjang(namaDepan, namaBelakang);
+  print(namaPanjang); // Output: Ballerina Cappuccina
 }
